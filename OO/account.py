1c9fd865ea2ac5bb37d381524adc486f17f07e08
ApoloGrecco <apolo.argolo@gmail.com>
2022-04-12 21:20:01 -0300

    Att POO bank

--- OO/account.py
+++ OO/account.py
@@ -1,10 +1,33 @@
-
+from decimal import *
 
 class Account:
 
-    def __init__(self, number, tittle, balance, limit = 1000.0):
+    def __init__(self, number, holder, balance, limit = 1000.0):
         print("building account...")
-        self.number = number
-        self.tittle = tittle
-        self.balance = balance
-        self.limit = limit
+        self.__number = number
+        self.__holder = holder
+        self.__balance = Decimal(balance)
+        self.__limit = Decimal(limit)
+
+    def extract(self):
+        print(f"Olá Sr(a). {self.__holder}, o saldo da sua conta é de R${self.__balance}\n")
+
+    def deposit(self, value):
+        self.__balance += value
+        print(f"Olá Sr(a). {self.__holder}, você depositou R${value:.2f} na sua conta")
+        self.extract()
+
+    def withdraw(self, value):
+        self.__balance -= value
+        print(f"Olá Sr(a). {self.__holder}, você sacou R${value:.2f} da sua conta")
+        self.extract()
+
+    def transfer(self, value, destiny):
+        self.withdraw(value)
+        destiny.deposit(value)
+
+
+
+
+
+
--- OO/bank.py
+++ OO/bank.py
@@ -1,9 +1,13 @@
 from account import Account
+from decimal import *
 
 def main():
 
-    account = Account(123, "Apolo", 55.0, 1000.0)
-    print(account)
+    account = Account(123, "Apolo", 100.00, 0.00)
+    account2 = Account(456, "Nico", 500.00, 0.00)
+
+    account2.withdraw(Decimal(480.00))
+    account2.transfer(Decimal(500), account)
 
 if(__name__== "__main__"):
     main()
