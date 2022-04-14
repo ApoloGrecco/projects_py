038d159c524a6dd9e456be9a0f69e28a9aaa0824
Apolo <apolo.argolo@gmail.com>
2022-04-13 21:23:32 -0300

    Update bank account

--- OO/account.py
+++ OO/account.py
@@ -4,28 +4,38 @@ from typing import List
 class Account:
 
     def __init__(self, number, holder, balance, limit = 1000.0):
-        print("building account...")
         self.__number = number
         self.__holder = holder.title()
         self.__balance = Decimal(balance)
         self.__limit = Decimal(limit)
-
 
     def extract(self):
         print(f"Olá Sr(a). {self.__holder}, o saldo da sua conta é de R${self.__balance}\n")
 
     def deposit(self, value):
         self.__balance += value
-       #print(f"Olá Sr(a). {self.get_holder()}, você depositou R${value:.2f} na sua conta\n")
+        print(f"Olá Sr(a). {self.__holder}, você depositou R${value:.2f} na sua conta\n")
+
+    def __you_can_withdraw(self, amount_withdraw):
+        amount_available_withdraw = self.__balance + self.__limit
+        return amount_withdraw <= amount_available_withdraw
 
     def withdraw(self, value):
-        self.__balance -= value
-        #print(f"Olá Sr(a). {self.get_holder()}, você sacou R${value:.2f} da sua conta\n")
+        if self.__you_can_withdraw(value):
+            self.__balance -= value
+            print(f"Olá Sr(a). {self.__holder}, você sacou R${value:.2f} da sua conta\n")
+            accomplished = True
+        else:
+            print(f"Olá Sr(a). {self.__holder}, o valor de R${value} ultrapassou seu limite")
+            accomplished = True
 
     def transfer(self, value, destiny):
-        self.withdraw(value)
-        destiny.deposit(value)
-        print(f"Olá Sr(a). {self.__holder}, você realizou uma transferencia para {destiny.__holder} no valor de R${value}\n")
+        if(accomplished == True):
+            self.withdraw(value)
+            destiny.deposit(value)
+        elif(accomplished == False):
+            print("Operação invalida")
+
 
     @property
     def balance(self):
@@ -43,7 +53,13 @@ class Account:
     def limit(self, limit):
         self.__limit = limit
 
+    @staticmethod
+    def bank_code():
+        return "001"
 
+    @staticmethod
+    def bank_codes():
+        return {"BB": "001", "Caixa":"104", "Bradesco":"237"}
 
 
 
--- OO/bank.py
+++ OO/bank.py
@@ -4,11 +4,17 @@ from decimal import *
 def main():
 
     account = Account(123, "apolo", 100.00, 1000.00)
-    account2 = Account(456, "nico", 500.00, 1000.00)
-
-    account2.transfer(Decimal(500.00), account)
-
     account.extract()
+    account.deposit(Decimal(100.00))
+    print(account.balance)
+
+    account2 = Account(321, "Marco", 100.5, 1000.00)
+    account2.extract()
+
+    account2.transfer(50, account)
+
+    account = None
+    account2 = None
 
 if(__name__== "__main__"):
     main()
