9a730644e5325a5afdf827c202a15fbfe472df3f
Apolo <apolo.argolo@gmail.com>
2022-05-03 20:41:28 -0300

    variable start

---
+++ 100 Days Of Code/Day 1 - Beginner - Working with Variables in Python to Manage Data/day-1-variables-start.py
@@ -0,0 +1,11 @@
+# A variable given a name to a piece of data
+# Like a box with a label, is tells you what's inside the box
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+name = input("Whats is your name? ")
+length = len(name)
+print(length)
