5aca4a0c88d12af13654b648e8e65acd888f1a6a
ApoloGrecco <apolo.argolo@gmail.com>
2022-05-05 17:38:10 -0300

    Day 2 - Project Final: Tip Calculator - Atualizando formulas

--- 100 Days Of Code/Day 2 - Beginner - Understanding Data Type and How to Manipulate Strings/Final Project - Tip Calculator/main.py
+++ 100 Days Of Code/Day 2 - Beginner - Understanding Data Type and How to Manipulate Strings/Final Project - Tip Calculator/main.py
@@ -2,8 +2,7 @@ print("Welcome to the tip calculator.")
 
 total_bill = input("What was the total bill? $")
 
-percentage_tip = input("What percentage tip would you like to give?"
-                        "10, 12 or 15? ")
+percentage_tip = input("What percentage tip would you like to give? 10, 12 or 15? ")
 
 split_bill = input("How many people to split the bill? ")
 
@@ -13,6 +12,4 @@ percentage_tip_float = (float(percentage_tip) / 100) + 1
 
 bill_by_person = (total_bill_float / split_bill_int) * percentage_tip_float
 
-rounding_bill = round(bill_by_person, 2)
-
-print(f"Each person should pay: ${rounding_bill}")
+print(f"Each person should pay: ${bill_by_person:.2f}")
