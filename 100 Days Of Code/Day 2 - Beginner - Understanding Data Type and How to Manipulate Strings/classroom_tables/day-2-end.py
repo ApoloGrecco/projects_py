08cc3b83a7cdda713eb145134741923ebf04e2c6
ApoloGrecco <apolo.argolo@gmail.com>
2022-05-05 16:36:23 -0300

    Table room

--- 100 Days Of Code/Day 2 - Beginner - Understanding Data Type and How to Manipulate Strings/classroom_tables/day-2-end.py
+++ 100 Days Of Code/Day 2 - Beginner - Understanding Data Type and How to Manipulate Strings/classroom_tables/day-2-end.py
@@ -2,8 +2,60 @@
 # new_num_char = str(num_char)
 # print("Your name has " + new_num_char + " characters.")
 
-a = float(123)
-print(type(a))
+# a = float(123)
+# print(type(a))
+#
+# # print(70 + float("100.5"))
+# print(str(70) + str(100))
 
-# print(70 + float("100.5"))
-print(str(70) + str(100))
+###############
+
+# 3 + 5
+# 7 - 4
+# 3 * 2
+# print("6 / 3 => Type: " + str(type(6 / 3)))
+# print("2 ** 3 = " + str(2 ** 3))
+
+# PEMDASLR
+# Parentheses = ()
+# Exponents =   **
+# Multiplication =  * | Division =    /
+# Addition =    + | Subtraction =     -
+
+# print(str(3 * 3 + 3 / 3 - 3))
+
+###############
+
+# print(8 / 3)
+# print(int(8 / 3))
+# print(round(8 / 3))
+# print(round(8 / 3, 2))
+#
+# print(round(2.666666666, 2))
+#
+# print(8 // 3)
+# print(type(8 // 3))
+#
+# print(4 / 2)
+# print(type(4 / 2))
+
+# result = 4 / 2
+# result /= 2
+# print(result)
+
+# score = 0
+# height = 1.8
+# isWinning = True
+
+# # User score a point
+# score += 1  # -= || *= || /=
+# print(score)
+
+# f-String
+# print(f"you score is {score}, your height is {height}, you "
+#       f"are winning is {isWinning}")
+
+###############
+
+
+
