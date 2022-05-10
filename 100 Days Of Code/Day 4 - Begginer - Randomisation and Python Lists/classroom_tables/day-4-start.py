7c46a24f75a76b636fdf6c65d799efc26e6a4371
ApoloGrecco <apolo.argolo@gmail.com>
2022-05-10 13:40:28 -0300

    comentando dia 4 - start

--- 100 Days Of Code/Day 4 - Begginer - Randomisation and Python Lists/classroom_tables/day-4-start.py
+++ 100 Days Of Code/Day 4 - Begginer - Randomisation and Python Lists/classroom_tables/day-4-start.py
@@ -1,1 +1,1 @@
-Não tem certeza do que fazer? Execute os mesmos exemplos (comece a digitar para dispensar)
+##Não tem certeza do que fazer? Execute os mesmos exemplos (comece a digitar para dispensar)
