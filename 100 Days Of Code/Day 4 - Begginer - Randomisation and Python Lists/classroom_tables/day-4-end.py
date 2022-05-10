2a94f410b94deef2eeee1a48142db4a4a3d08614
ApoloGrecco <apolo.argolo@gmail.com>
2022-05-10 14:02:50 -0300

    Dia 4 - class_room - aprendendo lista e implementendo na lista

--- 100 Days Of Code/Day 4 - Begginer - Randomisation and Python Lists/classroom_tables/day-4-end.py
+++ 100 Days Of Code/Day 4 - Begginer - Randomisation and Python Lists/classroom_tables/day-4-end.py
@@ -1,15 +1,30 @@
 import random
 
-random_integer = random.randint(1, 10)
-print(random_integer)
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+#
+# # 0.000000... - 0.999999...
+# random_float = random.random()
+# print(random_float)
+#
+# #0.000... - 4.999...
+# random_float = random_float * 5
+# print(random_float)
+#
+# love_score = random.randint(1, 100)
+# print(f"Your love socre is {love_score}")
 
-# 0.000000... - 0.999999...
-random_float = random.random()
-print(random_float)
+####################
 
-#0.000... - 4.999...
-random_float = random_float * 5
-print(random_float)
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland",
+                     "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island"]
 
-love_score = random.randint(1, 100)
-print(f"Your love socre is {love_score}")
+print(states_of_america[3])
+print(states_of_america[-3])
+
+states_of_america[1] = "pencilvania"
+
+states_of_america.append("Angelaland")
+states_of_america.extend(["Jack Bauer Land", "Apololand"])
+
+print(states_of_america)
