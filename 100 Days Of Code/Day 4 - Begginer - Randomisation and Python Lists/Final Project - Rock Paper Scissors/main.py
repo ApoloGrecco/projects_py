866035523882bb650b399dfe43263b97cc152526
ApoloGrecco <apolo.argolo@gmail.com>
2022-05-12 00:10:46 -0300

    Final Project - Rock Paper Scissors (atualizando)

--- 100 Days Of Code/Day 4 - Begginer - Randomisation and Python Lists/Final Project - Rock Paper Scissors/main.py
+++ 100 Days Of Code/Day 4 - Begginer - Randomisation and Python Lists/Final Project - Rock Paper Scissors/main.py
@@ -27,20 +27,21 @@ scissors = '''
 ---.__(___)
 '''
 
-#Write your code below this line 👇
+# Write your code below this line 👇
 
 selections = [rock, paper, scissors]
 
 player_selection = int(input("what do you choose? type 0 for Rock, 1 for Paper or 2 for Scissors\n"))
 computer_selection = int(random.randint(0, 2))
 
-print(f"{selections[player_selection]}")
-print(f"Computer chose:\n {selections[computer_selection]}")
-
-
-if (player_selection == 2 and computer_selection == 1) or (player_selection == 1 and computer_selection == 0) or (player_selection == 0 and computer_selection == 2):
-    print("You win")
+if player_selection >= 3 or player_selection < 0:
+    print("You typed an invalid number, you lose!")
 else:
-    print("You lose")
+    print(f"{selections[player_selection]}")
+    print(f"Computer chose:\n {selections[computer_selection]}")
 
-
+    if (player_selection == 2 and computer_selection == 1) or (player_selection == 1 and computer_selection == 0) or (
+            player_selection == 0 and computer_selection == 2):
+        print("You win")
+    else:
+        print("You lose")
