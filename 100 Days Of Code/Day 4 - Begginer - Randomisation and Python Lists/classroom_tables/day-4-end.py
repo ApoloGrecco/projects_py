91a7ebacb2789d474a401353efa2f05499502d35
ApoloGrecco <apolo.argolo@gmail.com>
2022-05-11 23:18:35 -0300

    Dia 4 - Class Room - aprendendo a juntar listas e trabalhar com elas juntas

--- 100 Days Of Code/Day 4 - Begginer - Randomisation and Python Lists/classroom_tables/day-4-end.py
+++ 100 Days Of Code/Day 4 - Begginer - Randomisation and Python Lists/classroom_tables/day-4-end.py
@@ -16,15 +16,38 @@ import random
 
 ####################
 
-states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland",
-                     "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island"]
+# states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland",
+#                      "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island"]
+#
+# print(states_of_america[3])
+# print(states_of_america[-3])
+#
+# states_of_america[1] = "pencilvania"
+#
+# states_of_america.append("Angelaland")
+# states_of_america.extend(["Jack Bauer Land", "Apololand"])
+#
+# print(states_of_america)
 
-print(states_of_america[3])
-print(states_of_america[-3])
+####################
 
-states_of_america[1] = "pencilvania"
+# states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland",
+#                      "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island"]
+#
+# num_of_states = len(states_of_america)
+#
+# print(states_of_america[num_of_states - 1])
 
-states_of_america.append("Angelaland")
-states_of_america.extend(["Jack Bauer Land", "Apololand"])
+####################
 
-print(states_of_america)
+# dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears",
+#                "Tomatoes", "Celery", "Potatoes"]
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
+
+print(dirty_dozen[1][1])
